6a3dc5054e6ff1a3a3c585261d141c08b4c28cfb
Shayan <55254368+ShayanFF@users.noreply.github.com>
2023-06-17 01:12:24 -0400

    Revert main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def processImg(imgPath1, imgPath2):
     img1rgb = cvtColor(img1, COLOR_BGR2RGB)
     img2rgb = cvtColor(img2, COLOR_BGR2RGB)
 
-    img1encoded = face_recognition.face_encodings(img1rgb)
-    img2encoded = face_recognition.face_encodings(img2rgb)
+    img1encoded = face_recognition.face_encodings(img1rgb)[0]
+    img2encoded = face_recognition.face_encodings(img2rgb)[0]
 
     return face_recognition.compare_faces([img1encoded], img2encoded)
